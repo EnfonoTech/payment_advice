cc9e23076fee4805546bb6dd31457249b1b1f63d
AravindR97 <aravindrajeshchry@gmail.com>
2025-05-21 12:40:10 +0530

    feat(Payment Advice Reference): Fetch amounts from reference records

--- payment_advice/payment_advice/doctype/payment_advice/payment_advice.js
+++ payment_advice/payment_advice/doctype/payment_advice/payment_advice.js
@@ -6,3 +6,30 @@
 
 // 	},
 // });
+
+frappe.ui.form.on('Payment Advice', {
+    refresh: function(frm) {
+    },
+});
+
+frappe.ui.form.on('Payment Advice Reference', {
+    reference_record: function(frm, cdt, cdn) {
+        const row = frappe.get_doc(cdt, cdn);
+        
+        if (!row.reference_doctype || !row.reference_record) {
+            frappe.model.set_value(cdt, cdn, 'amount', 0);
+            return;
+        }
+
+        frappe.db.get_value(
+            row.reference_doctype,
+            row.reference_record,
+            'grand_total',
+            (r) => {
+                if (r && r.grand_total) {
+                    frappe.model.set_value(cdt, cdn, 'amount', r.grand_total);
+                }
+            }
+        );
+    }
+});
