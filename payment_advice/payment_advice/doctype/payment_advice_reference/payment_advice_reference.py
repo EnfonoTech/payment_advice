1be5b7b35340af51be1592b461998f36a50517b0
AravindR97 <aravindrajeshchry@gmail.com>
2025-05-21 12:04:41 +0530

    Add Payment advice doctype

---
+++ payment_advice/payment_advice/doctype/payment_advice_reference/payment_advice_reference.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Enfono and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PaymentAdviceReference(Document):
+	pass
