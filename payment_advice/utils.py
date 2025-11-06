a97610b6c5bc76b2d9c72b5b1902051f59186e39
AravindR97 <aravindrajeshchry@gmail.com>
2025-11-06 12:46:55 +0530

    feat(Payment Advice): Set Payment Entry Reference on submitting linked Payment Entry

--- payment_advice/utils.py
+++ payment_advice/utils.py
@@ -7,3 +7,13 @@ def validate_reference_no(doc, method):
             "Payment Entry cannot be submitted with reference number 'TEMP'. Please update the reference number and date",
             title="Invalid Reference Number"
         )
+
+    set_payment_entry_reference_in_payment_advice(doc, method)
+    
+
+def set_payment_entry_reference_in_payment_advice(doc, method):
+    """Set Payment Entry reference in Payment Advice"""
+    if doc.custom_payment_advice:
+        payment_advice = frappe.get_doc("Payment Advice", doc.custom_payment_advice)
+        payment_advice.payment_entry_reference = doc.name
+        payment_advice.save(ignore_permissions=True)
