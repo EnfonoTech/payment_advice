2c86467fc6a0087fbe27f6dff157f33dd31eaf5b
AravindR97 <aravindrajeshchry@gmail.com>
2026-02-13 15:12:33 +0530

    fix: Set row total for Expense Claim

--- payment_advice/payment_advice/doctype/payment_advice/payment_advice.js
+++ payment_advice/payment_advice/doctype/payment_advice/payment_advice.js
@@ -339,6 +339,11 @@ frappe.ui.form.on('Payment Advice Reference', {
                             frappe.model.set_value(cdt, cdn, 'exchange_rate', r.conversion_rate);
 
                             if (r.grand_total){
+
+                                if(row.reference_doctype == "Expense Claim"){
+                                    frappe.model.set_value(cdt, cdn, 'amount', r.grand_total);
+                                }
+
                                 frappe.model.set_value(cdt, cdn, 'amount_in_currency', r.grand_total);
                                 if (r.outstanding_amount && r.outstanding_amount != null) {
                                     frappe.model.set_value(cdt, cdn, 'net_payable_amount_in_currency', r.outstanding_amount/r.conversion_rate);
